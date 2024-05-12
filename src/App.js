d258bb74871cedf9aceb3322a38156492d977fe2
mahya27h <faride27567@gmail.com>
2024-05-12 13:29:44 +0330

    resolv

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import Postlist from './components/Postlist';
 
 function App() {
   const user = [
-    {id:1, name:kaveh},
-    {id:2, name:ali},
-    {id:3, name:reza},
-  3
+    {id: 1, name: 'kaveh'},
+    {id: 2, name: 'ali'},
+    {id: 3, name: 'reza'},
+ 
   ]
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <Postlist {...user[0]}/>
-      <Postlist {...user[1]}/>
-      <Postlist {...user[2]}/>
+      <Postlist {...user[0]} />
+      <Postlist {...user[1]} />
+      <Postlist {...user[2]} />
     </div>
   );
 }
